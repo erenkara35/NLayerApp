52b9cddbe3cb958fe4d7dc4d2d6fbd2e9db29668
eren.kara <eren.kara@OVA10ERENK-NB>
2023-11-11 13:55:53 +0300

    Add project files.

---
+++ NLayer.Core/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    //abstract = Soyut nesne (Yeni bir nesne türetilemez. Core katmanında kullanıma uygundur)
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
